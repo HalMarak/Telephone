2021e73870846825da87f488d27da817eff6c512
OleshkoMV <oleshkomv@DOM201.LOCAL>
2022-11-26 10:40:11 +0700

    Initial commit

---
+++ MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.telephone
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.telephone.R
+import android.content.ContentValues
+import android.database.Cursor
+
+class MainActivity : AppCompatActivity() {
+    private val list = mutableListOf<Todo>()
+    private lateinit var adapter: RecyclerAdapter
+    val dbHelper = DBHelper(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val editText = findViewById<EditText>(R.id.editText)
+
+        list.addAll(dbHelper.getAll())
+
+        adapter = RecyclerAdapter(list) {
+            val todo = list[it].id
+            dbHelper.remove( list[it].id)
+            list.removeAt(it)
+            adapter.notifyItemRemoved(it)
+        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.spisok)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        val buttonAdd = findViewById<Button>(R.id.button)
+        buttonAdd.setOnClickListener {
+            val title = editText.text.toString()
+            val id = dbHelper.add(title)
+            list.add(Todo(id, title))
+            adapter.notifyItemInserted(list.lastIndex)
+
+
+        }
+    }
+}
---
+++ Todo.kt
@@ -0,0 +1,3 @@
+package com.example.telephone
+
+data class Todo(val id: Long, val title: String)
